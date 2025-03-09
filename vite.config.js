b0ceaf21aa00f213d23aad7264287681c03e5cb7
taitanvo2309@gmail.com <taitanvo2309@gmail.com>
2025-03-09 20:35:33 +0700

    Fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/",
+  plugins: [react(), tailwindcss()]
 });
