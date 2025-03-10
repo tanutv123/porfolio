24c16e593eced927ffe49138578b53e22bfa6e68
taitanvo2309@gmail.com <taitanvo2309@gmail.com>
2025-03-10 13:19:37 +0700

    Fix content

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -11,7 +11,9 @@ export const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    console.log(import.meta.env.VITE_SERVICE_ID);
+    console.log(import.meta.env.VITE_TEMPLATE_ID);
+    console.log(import.meta.env.VITE_PUBLIC_KEY);
     emailjs
       .sendForm(
         import.meta.env.VITE_SERVICE_ID,
