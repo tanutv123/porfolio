a7281411b3d81c07447ea43035f9b8de8299cd59
taitanvo2309@gmail.com <taitanvo2309@gmail.com>
2025-03-11 10:33:16 +0700

    Fix content

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -19,7 +19,7 @@ export const Home = () => {
           </p>
           <div className="flex justify-center space-x-4">
             <a
-              href="#reactprojects"
+              href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
               View Projects
